e57f1357e155e0e30b818467f6d73cda344544f6
Benchaton <92680749+BenchatonDev@users.noreply.github.com>
2024-04-03 18:33:07 +0200

    Update script.js

--- common/script.js
+++ common/script.js
@@ -1,1 +1,4 @@
-
+let function togglenav() {
+  document.getElementById("topbarnavbtn").style.transform = "rotate(0deg)"
+  document.getElementById("topbarnavbtn").style.transition = "transform 0.2s ease"
+}
