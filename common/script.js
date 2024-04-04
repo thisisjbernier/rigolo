9f402accef790bac1e80bf76e44425a0e75f481d
Benchaton <92680749+BenchatonDev@users.noreply.github.com>
2024-04-04 10:23:28 +0200

    Update script.js

--- common/script.js
+++ common/script.js
@@ -1,12 +1,12 @@
 function togglenav(){
-  if (document.getElementById("navmen").style.width = "250") {
+  if (document.getElementById("navmen").style.width = "250px") {
         document.getElementById("topbarnavbtn").style.transform = "rotate(0deg)";
         document.getElementById("topbarnavbtn").style.transition = "transform 0.2s ease";
-        document.getElementById("navmen").style.width = "0";
+        document.getElementById("navmen").style.width = "0px";
   }
-  else if (document.getElementById("navmen").style.width = "0") {
+  else if (document.getElementById("navmen").style.width = "0px") {
         document.getElementById("topbarnavbtn").style.transform = "rotate(90deg)";
         document.getElementById("topbarnavbtn").style.transition = "transform 0.2s ease";
-        document.getElementById("navmen").style.width = "250";
+        document.getElementById("navmen").style.width = "250px";
   }
 }
