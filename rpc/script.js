2c0ed8c3211a19efe7f9b63c01141c3aba4fc015
Benchaton <92680749+BenchatonDev@users.noreply.github.com>
2024-04-11 10:40:53 +0200

    Update script.js

--- rpc/script.js
+++ rpc/script.js
@@ -1,1 +1,44 @@
+var plrscore = 0
+var botscore = 0
+document.getElementById("score").textContent = plrscore + " | " + botscore
 
+function choice(plrmove) {
+    var botmove = let x = Math.floor((Math.random() * 3) + 1)
+    // Code pas top mais azul
+    if (plrmove == 1 && botmove == 1) {
+        console.log("Tie")
+        return
+    }
+    else if (plrmove == 2 && botmove == 2) {
+        console.log("Tie")
+        return
+    }
+    else if (plrmove == 3 && botmove == 3) {
+        console.log("Tie")
+        return
+    }
+    else if (plrmove == 1 && botmove == 3) {
+        console.log("Player Win")
+        plrscore += 1
+        document.getElementById("score").textContent = plrscore + " | " + botscore
+        return
+    }
+    else if (plrmove == 2 && botmove == 1) {
+        console.log("Player Win")
+        plrscore += 1
+        document.getElementById("score").textContent = plrscore + " | " + botscore
+        return
+    }
+    else if (plrmove == 3 && botmove == 2) {
+        console.log("Player Win")
+        plrscore += 1
+        document.getElementById("score").textContent = plrscore + " | " + botscore
+        return
+    }
+    else {
+        console.log("Bot Win")
+        botscore += 1
+        document.getElementById("score").textContent = plrscore + " | " + botscore
+        return
+    }
+}
