79cfb09837254c2747e56175f27bbd9c5bc382b9
Benchaton <92680749+BenchatonDev@users.noreply.github.com>
2024-04-11 18:32:49 +0200

    Update script.js

--- rpc/script.js
+++ rpc/script.js
@@ -1,50 +1,74 @@
+let plroptions = [document.getElementsByClassName("rock"), document.getElementsByClassName("paper"), document.getElementsByClassName("scissors")]
+let botoptions = [document.getElementsByClassName("botrock"), document.getElementsByClassName("botpaper"), document.getElementsByClassName("botscissors")]
 var plrscore = 0
 var botscore = 0
+var
 
 function choice(plrmove) {
-    var botmove = Math.floor((Math.random() * 3) + 1)
     // Code pas top mais azul
+    var botmove = Math.floor((Math.random() * 3) + 1)
+
+    // Fait disparaître les élements non selectionner du joueur :)
+    if (plrmove == 1){
+        plroptions[1].style.display = "none"
+        plroptions[2].style.display = "none" 
+    }
+    else if (plrmove == 2) {
+        plroptions[0].style.display = "none"
+        plroptions[2].style.display = "none" 
+    }
+    else if (plrmove == 3) {
+        plroptions[0].style.display = "none"
+        plroptions[1].style.display = "none" 
+    }
+
+    // Fait disparaître les élements non selectionner du bot :)
+    if (botmove == 1){
+        botoptions[1].style.display = "none"
+        botoptions[2].style.display = "none" 
+    }
+    else if (botmove == 2) {
+        botoptions[0].style.display = "none"
+        botoptions[2].style.display = "none" 
+    }
+    else if (botmove == 3) {
+        botoptions[0].style.display = "none"
+        botoptions[1].style.display = "none" 
+    }
+    
+// La logique du jeux codé nullement mais genre vrm
     if (plrmove == 1 && botmove == 1) {
         document.getElementById("wtext").textContent = "😐 Égalité 😐"
         document.getElementById("wtext").style.color = "#000"
-        return
     }
     else if (plrmove == 2 && botmove == 2) {
         document.getElementById("wtext").textContent = "😐 Égalité 😐"
         document.getElementById("wtext").style.color = "#000"
-        return
     }
     else if (plrmove == 3 && botmove == 3) {
         document.getElementById("wtext").textContent = "😐 Égalité 😐"
         document.getElementById("wtext").style.color = "#000"
-        return
     }
     else if (plrmove == 1 && botmove == 3) {
         document.getElementById("wtext").textContent = "🎉 Vous avez gagné 🎉"
         document.getElementById("wtext").style.color = "#f1c40f"
         plrscore += 1
-        document.getElementById("score").textContent = plrscore + " | " + botscore
-        return
     }
     else if (plrmove == 2 && botmove == 1) {
         document.getElementById("wtext").textContent = "🎉 Vous avez gagné 🎉"
         document.getElementById("wtext").style.color = "#f1c40f"
         plrscore += 1
-        document.getElementById("score").textContent = plrscore + " | " + botscore
-        return
     }
     else if (plrmove == 3 && botmove == 2) {
         document.getElementById("wtext").textContent = "🎉 Vous avez gagné 🎉"
         document.getElementById("wtext").style.color = "#f1c40f"
         plrscore += 1
-        document.getElementById("score").textContent = plrscore + " | " + botscore
-        return
     }
     else {
         document.getElementById("wtext").textContent = "😔 Vous avez perdu 😔"
         document.getElementById("wtext").style.color = "#e74c3c"
         botscore += 1
-        document.getElementById("score").textContent = plrscore + " | " + botscore
-        return
     }
+
+    document.getElementById("score").textContent = plrscore + " | " + botscore
 }
