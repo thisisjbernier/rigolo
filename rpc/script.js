f8f03e579af749ce5f10e9579072d354e6522ae1
Benchaton <92680749+BenchatonDev@users.noreply.github.com>
2024-04-11 18:03:44 +0200

    Update script.js

--- rpc/script.js
+++ rpc/script.js
@@ -5,37 +5,44 @@ function choice(plrmove) {
     var botmove = Math.floor((Math.random() * 3) + 1)
     // Code pas top mais azul
     if (plrmove == 1 && botmove == 1) {
-        console.log("Tie")
+        document.getElementById("score").textContent = "😐 Égalité 😐"
+        document.getElementById("score").style.color = "#000"
         return
     }
     else if (plrmove == 2 && botmove == 2) {
-        console.log("Tie")
+        document.getElementById("score").textContent = "😐 Égalité 😐"
+        document.getElementById("score").style.color = "#000"
         return
     }
     else if (plrmove == 3 && botmove == 3) {
-        console.log("Tie")
+        document.getElementById("score").textContent = "😐 Égalité 😐"
+        document.getElementById("score").style.color = "#000"
         return
     }
     else if (plrmove == 1 && botmove == 3) {
-        console.log("Player Win")
+        document.getElementById("score").textContent = "🎉 Vous avez gagné 🎉"
+        document.getElementById("score").style.color = "#f1c40f"
         plrscore += 1
         document.getElementById("score").textContent = plrscore + " | " + botscore
         return
     }
     else if (plrmove == 2 && botmove == 1) {
-        console.log("Player Win")
+        document.getElementById("score").textContent = "🎉 Vous avez gagné 🎉"
+        document.getElementById("score").style.color = "#f1c40f"
         plrscore += 1
         document.getElementById("score").textContent = plrscore + " | " + botscore
         return
     }
     else if (plrmove == 3 && botmove == 2) {
-        console.log("Player Win")
+        document.getElementById("score").textContent = "🎉 Vous avez gagné 🎉"
+        document.getElementById("score").style.color = "#f1c40f"
         plrscore += 1
         document.getElementById("score").textContent = plrscore + " | " + botscore
         return
     }
     else {
-        console.log("Bot Win")
+        document.getElementById("score").textContent = "😔 Vous avez perdu 😔"
+        document.getElementById("score").style.color = "#e74c3c"
         botscore += 1
         document.getElementById("score").textContent = plrscore + " | " + botscore
         return
