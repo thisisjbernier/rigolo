70df14245975b5f1e35ac037100eac3eadc89d94
Benchaton <92680749+BenchatonDev@users.noreply.github.com>
2024-04-11 18:08:33 +0200

    Update script.js

--- rpc/script.js
+++ rpc/script.js
@@ -5,44 +5,44 @@ function choice(plrmove) {
     var botmove = Math.floor((Math.random() * 3) + 1)
     // Code pas top mais azul
     if (plrmove == 1 && botmove == 1) {
-        document.getElementById("score").textContent = "😐 Égalité 😐"
-        document.getElementById("score").style.color = "#000"
+        document.getElementById("wtext").textContent = "😐 Égalité 😐"
+        document.getElementById("wtext").style.color = "#000"
         return
     }
     else if (plrmove == 2 && botmove == 2) {
-        document.getElementById("score").textContent = "😐 Égalité 😐"
-        document.getElementById("score").style.color = "#000"
+        document.getElementById("wtext").textContent = "😐 Égalité 😐"
+        document.getElementById("wtext").style.color = "#000"
         return
     }
     else if (plrmove == 3 && botmove == 3) {
-        document.getElementById("score").textContent = "😐 Égalité 😐"
-        document.getElementById("score").style.color = "#000"
+        document.getElementById("wtext").textContent = "😐 Égalité 😐"
+        document.getElementById("wtext").style.color = "#000"
         return
     }
     else if (plrmove == 1 && botmove == 3) {
-        document.getElementById("score").textContent = "🎉 Vous avez gagné 🎉"
-        document.getElementById("score").style.color = "#f1c40f"
+        document.getElementById("wtext").textContent = "🎉 Vous avez gagné 🎉"
+        document.getElementById("wtext").style.color = "#f1c40f"
         plrscore += 1
         document.getElementById("score").textContent = plrscore + " | " + botscore
         return
     }
     else if (plrmove == 2 && botmove == 1) {
-        document.getElementById("score").textContent = "🎉 Vous avez gagné 🎉"
-        document.getElementById("score").style.color = "#f1c40f"
+        document.getElementById("wtext").textContent = "🎉 Vous avez gagné 🎉"
+        document.getElementById("wtext").style.color = "#f1c40f"
         plrscore += 1
         document.getElementById("score").textContent = plrscore + " | " + botscore
         return
     }
     else if (plrmove == 3 && botmove == 2) {
-        document.getElementById("score").textContent = "🎉 Vous avez gagné 🎉"
-        document.getElementById("score").style.color = "#f1c40f"
+        document.getElementById("wtext").textContent = "🎉 Vous avez gagné 🎉"
+        document.getElementById("wtext").style.color = "#f1c40f"
         plrscore += 1
         document.getElementById("score").textContent = plrscore + " | " + botscore
         return
     }
     else {
-        document.getElementById("score").textContent = "😔 Vous avez perdu 😔"
-        document.getElementById("score").style.color = "#e74c3c"
+        document.getElementById("wtext").textContent = "😔 Vous avez perdu 😔"
+        document.getElementById("wtext").style.color = "#e74c3c"
         botscore += 1
         document.getElementById("score").textContent = plrscore + " | " + botscore
         return
